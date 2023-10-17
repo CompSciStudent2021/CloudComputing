f8cf7cda00e839dd82a08369d2f165f949a98dc8
Ryan Francis <C21730391@mytudublin.ie>
2023-10-17 21:35:16 +0100

    Finished functionality and styling

---
+++ src/routes/deleteAllItems.js
@@ -0,0 +1,11 @@
+const db = require('../persistence');
+
+module.exports = async (req, res) => {
+  try {
+    await db.deleteAllItems();
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
